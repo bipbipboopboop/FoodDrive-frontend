49d98cf0d0affd2441927d06d665a9a1d74ef651
Ethan <email@email.com>
2022-06-26 22:54:18 +0800

    Refactored Codebase

--- src/Components/Cards/MenuCard.js
+++ src/Components/Cards/MenuCard.js
@@ -1,58 +1,24 @@
 import React, { useState } from "react";
 
 import styled from "styled-components";
+import useModal from "../../Hooks/useModal";
 
 // import OrderModal from "./OrderModal";
-import OrderModal from "../Modals/Cart.modal";
-
-const Card = styled.div`
-  width: 300px;
-  height: 400px;
-  padding: 10px;
-  margin: 20px;
-  border: solid;
-  border-radius: 20px;
-  background-color: white;
-  > * {
-    background-color: white;
-  }
-`;
-
-const CardImg = styled.img`
-  height: 50%;
-`;
+import OrderModal from "../Modals/Order.modal";
+import {
+  AbstractCard,
+  RedButton,
+  CardImg,
+  CardActions,
+} from "../Styles/styles";
 
 const CardContent = styled.div``;
 
-const CardActions = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-const Button = styled.button`
-  display: inline-block;
-  background-color: #d30000;
-  text-transform: uppercase;
-  color: white;
-  border-radius: 30px;
-  padding-top: 16px;
-  padding-right: 24px;
-  padding-bottom: 16px;
-  padding-left: 24px;
-  font-size: 16px;
-  text-align: center;
-  border: 0px;
-`;
 export default function MenuCard({ title, description, price, image }) {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
+  const { open, handleOpen, handleClose } = useModal();
+
   return (
-    <Card>
+    <AbstractCard>
       <CardImg src={image} />
       <CardContent>
         <h3>{title}</h3>
@@ -60,7 +26,7 @@ export default function MenuCard({ title, description, price, image }) {
         <p>${price}</p>
       </CardContent>
       <CardActions>
-        <Button onClick={handleOpen}>Select</Button>
+        <RedButton onClick={handleOpen}>Select</RedButton>
       </CardActions>
       <OrderModal
         title={title}
@@ -70,6 +36,6 @@ export default function MenuCard({ title, description, price, image }) {
         open={open}
         handleClose={handleClose}
       />
-    </Card>
+    </AbstractCard>
   );
 }
